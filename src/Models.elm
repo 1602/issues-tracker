9eab5393e0708f8985f1ee5d1e92b2375dbf4c76
anatoliy <mail@anatoliy.in>
2017-07-24 19:00:56 +0100

    Cleanup models

--- src/Models.elm
+++ src/Models.elm
@@ -1,51 +1,14 @@
 module Models exposing (..)
 
-import Date
-import Dict
-import Navigation exposing (Location)
-import Data.Issue exposing (Issue)
 import Data.Milestone exposing (Milestone)
 import Data.User exposing (User)
-import Data.Column exposing (Column(..))
 import Data.PersistentData exposing (PersistentData)
 import Pages.Repos
 import Pages.Roadmap
+import Pages.Board
+import Navigation exposing (Location)
 
 
-type IssueState = OpenIssue | ClosedIssue
-
-
-type alias Model =
-    { version : String
-    , user : Maybe User
-    , token : String
-    , repo : (String, String)
-    , location : Location
-    , now : Date.Date
-    , error : Maybe String
-    , currentIssues : Maybe (List Issue)
-    , iceboxIssues : Maybe (List Issue)
-    , closedIssues : Maybe (List Issue)
-    , milestones : Dict.Dict (String, String) (Dict.Dict String ExpandedMilestone)
-    , pickMilestoneForIssue : Maybe Issue
-    , lockedIssueNumber : String
-    , highlightStory : String
-    , newMilestoneTitle : String
-    , newIssueTitle : String
-    , needFocus : Bool
-    , addIssueToColumn : Column
-    , addIssueToMilestone : String
-    , filter : Filter
-    , filterStoriesBy : String
-    , etags : Dict.Dict String String
-    , searchTerms : String
-    , searchResults : List Issue
-    , didSwitch : Bool
-    , persistentData : PersistentData
-    , repos : Pages.Repos.Model
-    , roadmap : Pages.Roadmap.Model
-    , listMilestones : List Milestone
-    }
 
 
 type RemoteData a
@@ -54,15 +17,3 @@ type RemoteData a
     | Loaded a
 
 
-type Filter
-    = All
-    | CreatedBy String
-    | AssignedTo String
-    | HasMentionOf String
-
-
-type alias ExpandedMilestone =
-    { milestone : Milestone
-    , openIssues : Maybe (List Issue)
-    , closedIssues : Maybe (List Issue)
-    }
